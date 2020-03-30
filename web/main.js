ef863f8f44f8e7d020121297c3dcaddfe2f85dd8
Jelle Akkerman <jellea@users.noreply.github.com>
2020-03-30 22:16:28 +0200

    adding share and download fns

--- web/main.js
+++ web/main.js
@@ -2,7 +2,17 @@ var audio = new Audio();
 var data = [];
 var theme;
 var compile;
+var wave = new RIFFWAVE();
 audio.loop = true;
+
+const getQParam = (param) =>
+  decodeURI(atob(new URL(window.location).searchParams.get(param)));
+
+const setQParam = (param, val) => {
+  let url = new URL(window.location);
+  url.searchParams.set(param, encodeURI(btoa(val)));
+  window.history.pushState({ path: url.href }, "", url.href);
+};
 
 window.onload = () => {
   theme = new Theme();
@@ -21,7 +31,6 @@ window.onload = () => {
         data[i] = vec.get(i);
       }
 
-      var wave = new RIFFWAVE();
       wave.header.bitsPerSample = 16;
       wave.header.sampleRate = 44100;
       wave.header.numChannels = 1;
@@ -34,13 +43,28 @@ window.onload = () => {
       audio.src = wave.dataURI;
       if (continuePlaying) audio.play();
     };
+    init();
+  };
 
+  window.setFreqs = (e) => {
+    setQParam("frequencies", e.value);
     read();
+  };
+
+  window.init = () => {
+    let f = (document.getElementById("freqs").value = getQParam("frequencies")),
+      i = (document.getElementById("dslIndex").value = getQParam("index")),
+      w = (document.getElementById("dslWaveforms").value = getQParam(
+        "waveforms"
+      ));
+    compile(i, "index");
+    compile(f, "waveforms");
     draw();
   };
 
-  window.compile = (e, destination) => {
-    let seq = cljs.core.clj__GT_js(wave_dsl.parser.parse(e.value));
+  window.compile = (txt, destination) => {
+    setQParam(destination, txt);
+    let seq = wave_dsl.parser.parse__GT_js(txt);
     document.getElementById(destination).value = seq.join(" ");
     read();
   };
@@ -62,4 +86,30 @@ const play = (e) => {
 const loopToggle = (e) => {
   audio.loop = !audio.loop;
   e.className = audio.loop ? "" : "off";
+};
+
+const dataURItoBlob = (dataURI) => {
+  // convert base64/URLEncoded data component to raw binary data held in a string
+  var byteString;
+  if (dataURI.split(",")[0].indexOf("base64") >= 0)
+    byteString = atob(dataURI.split(",")[1]);
+  else byteString = unescape(dataURI.split(",")[1]);
+
+  // separate out the mime component
+  let mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
+
+  // write the bytes of the string to a typed array
+  var ia = new Uint8Array(byteString.length);
+  for (var i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+
+  return new Blob([ia], { type: mimeString });
+};
+
+const download = () => saveAs(dataURItoBlob(wave.dataURI), "segmod.wav");
+
+const share = () => {
+  navigator.clipboard.writeText(window.location.href);
+  alert("link copied to clipboard!");
 };
